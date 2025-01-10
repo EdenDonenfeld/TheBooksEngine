2121fd2d3a0a7bb665bd43bcfdd247586b2b4e26
EdenDonenfeld <edendonenfeld@gmail.com>
2025-01-10 19:57:34 +0200

    Fix client folder to be treated as a regular folder

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from './Components/Home';
+
+function App() {
+  return (
+    <div className="bg-babyBlue h-screen flex flex-col">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Include all files in your `src` directory
+  ],
+  theme: {
+    extend: {
+      colors: {
+        babyBlue: 'rgba(48, 176, 199, 0.15)', // Custom color
+        darkerBlue: 'rgba(31, 157, 180, 0.75)', // Custom color
+        recBlue: 'rgba(204, 220, 222, 1)', // Custom color
+        borderBlue: 'rgba(31, 157, 180, 0.2)', // Custom color
+      },
+      fontFamily: {
+        croissant: ['Croissant One', 'sans-serif'], // Define the custom font
+        crimson: ['Crimson Pro', 'serif'], // Add the Crimson Pro font family
+      },
+    },
+  },
+  plugins: [],
+};
