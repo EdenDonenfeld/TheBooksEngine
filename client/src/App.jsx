48e39ac6ec16e373ff8fba3b0e035384be35fd4e
EdenDonenfeld <edendonenfeld@gmail.com>
2025-01-31 18:29:20 +0200

    fixed bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { SearchProvider } from './Components/SearchContext';
 import Home from './Components/Home';
 import BookDetails from './Components/BookDetails';
--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -4,7 +4,7 @@ import logo from '../logo-books.png';
 const Header = () => {
   return (
     <header className="bg-darkerBlue py-5 flex items-center justify-center space-x-4 px-10">
-      <a href="/" className="flex items-center space-x-4">
+      <a href="/TheBooksEngine" className="flex items-center space-x-4">
         <h1 className="text-white text-4xl font-croissant tracking-wider">
           The Books Engine
         </h1>
